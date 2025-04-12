9a8c8a76236657fee853d3de49681142a06e87eb
Jerry <jerryjohnc1@gmail.com>
2025-04-12 09:22:47 +0000

    roles updated

--- resources/views/emails/admin/register.blade.php
+++ resources/views/emails/admin/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.email')
 @section('content')
-    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">Dear {{$user->firstname}},</p>
+    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">Dear {{$user->first_name}},</p>
     <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">A user account has been created for you or your login details has been updated.</p>
     <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">Go to {{$url}} and login with the credentials below. </p>
     <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;"><b>Username: {{$user->email??$user->username}} </b></p>
